1a454ed61e7acbb65e7d616ab5f82f3dba7c07ad
dchem <jaey226@gmail.com>
2015-03-23 10:57:57 -0700

    Added jsonparser predicates for Requestor callback data

--- AtJsonParser.cpp
+++ AtJsonParser.cpp
@@ -20,6 +20,25 @@ Handle<Object> AtJsonParser::parse( const JSONNode &n ) {
              node_name == "atBarHistory" ||
              node_name == "atSymbol" ||
              node_name == "atLoginResponse" ||
+             node_name == "time" ||
+             node_name == "open" ||
+             node_name == "high" ||
+             node_name == "low" ||
+             node_name == "close" ||
+             node_name == "atTickHistory" ||
+             node_name == "offsetDatabaseDate" ||
+             node_name == "tradeLastPrice" ||
+             node_name == "quoteBidPrice" ||
+             node_name == "quoteAskPrice" ||
+             node_name == "atMarketMoversDb" ||
+             node_name == "recordSymbol" ||
+             node_name == "itemSymbol" ||
+             node_name == "itemLastPrice" ||
+             node_name == "atQuoteDb" ||
+             node_name == "dataItems" ||
+             node_name == "priceData" ||
+             node_name == "dateTime" ||
+             node_name == "atQuoteStream"
               ) {
             retData->Set( String::NewSymbol( node_name.c_str() ), parse( *i ) );
         }
@@ -45,22 +64,59 @@ Handle<Object> AtJsonParser::parse( const JSONNode &n ) {
 }
 
 bool AtJsonParser::filterAsString( const std::string& node_name ) {
-    if ( node_name == "messageId" )
+    if ( node_name == "messageId" ||
+         node_name == "name" ||
+         node_name == "type" ||
+         node_name == "sector" ||
+         node_name == "industry" ||
+         node_name == "symbolStr" ||
+         node_name == "tradeLastExchange" ||
+         node_name == "quoteBidExchange" ||
+         node_name == "quoteAskExchange" ||
+         node_name == "byte" ||
+         node_name == "string" ||
+         node_name == "unicodeString"
+       )
         return true;
     return false;
 }
 
 
 bool AtJsonParser::filterAsNumber( const std::string& node_name ) {
+    if ( node_name == "price"
+       )
+        return true;
     return false;
 }
 
 bool AtJsonParser::filterAsInteger( const std::string& node_name ) {
     if ( node_name == "hSession" ||
          node_name == "hOrigRequest" ||
+         node_name == "hRequest" ||
          node_name == "statusType" ||
          node_name == "loginResponse" || 
-         node_name == "hRequest" )
+         node_name == "origRequest" ||
+         node_name == "enum" ||
+         node_name == "count" ||
+         node_name == "precision" ||
+         node_name == "volume" ||
+         node_name == "symbolType" ||
+         node_name == "exchangeType" ||
+         node_name == "countryType" ||
+         node_name == "nextOffset" ||
+         node_name == "tradeLastSize" ||
+         node_name == "tradeCondition" ||
+         node_name == "quoteBidSize" ||
+         node_name == "quoteAskSize" ||
+         node_name == "quoteCondition" ||
+         node_name == "itemVolume" ||
+         node_name == "uint32" ||
+         node_name == "uint64" ||
+         node_name == "int32" ||
+         node_name == "int64" ||
+         node_name == "dataItemQuoteFieldType" ||
+         node_name == "dataItemFieldStatus"
+       )
         return true;
     return false;
 }
